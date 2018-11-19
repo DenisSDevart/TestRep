4cae4e747fd9618c5ff5e5442eecec8e240d4929
DenisSDevart <44638487+DenisSDevart@users.noreply.github.com>
2018-11-19 14:16:26 +0200

    Delete dbo.dept.sql

--- Tables/dbo.dept.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[dept]
-(
-[deptno] [int] NOT NULL,
-[dname] [varchar] (14) COLLATE Cyrillic_General_CI_AS NULL,
-[loc] [varchar] (13) COLLATE Cyrillic_General_CI_AS NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[dept] ADD CONSTRAINT [PK_dept] PRIMARY KEY CLUSTERED  ([deptno]) ON [PRIMARY]
-GO
